9214305e0afb5e84813a686f81fdffce8d699396
MohammadDwikat1 <mohammaddwikat0059@gmail.com>
2026-03-05 19:28:54 +0200

    test

--- testMobile/app.js
+++ testMobile/app.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { WebView } from "react-native-webview";
 import { SafeAreaView } from 'react-native-safe-area-context';
+import {ScrollView} from 'react-native';
 // You can import supported modules from npm
 import { Card } from 'react-native-paper';
 
